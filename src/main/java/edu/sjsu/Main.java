ebf8b31467db9d4a67e96bf6df426dbcc03d94e3
ardwaino <mr.shreyass@gmail.com>
2024-04-24 18:50:04 -0700

    commit

--- src/main/java/edu/sjsu/Main.java
+++
@@ -1,9 +0,0 @@
-package edu.sjsu;
-
-public class Main {
-    public static void main(String[] args) {
-        boolean result = NameChecker.check(args[0]);
-
-        System.out.println("Result: " + result);
-    }
-}
